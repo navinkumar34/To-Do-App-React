fe16a851e436397ee198b2e84e01393169d9b7bb
Navin Kumar <navinredu@gmail.com>
2020-09-29 18:48:10 +0530

    Updates

---
+++ precache-manifest.1e12c1772b3be667359126cf37df7f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403d7e61035b204c033a26bde50ce77f",
+    "url": "/To-Do-App-React/index.html"
+  },
+  {
+    "revision": "056285cb470e6f7755c3",
+    "url": "/To-Do-App-React/static/css/2.c11e63b2.chunk.css"
+  },
+  {
+    "revision": "1667039c22321bdff15b",
+    "url": "/To-Do-App-React/static/css/main.b9fd3d78.chunk.css"
+  },
+  {
+    "revision": "056285cb470e6f7755c3",
+    "url": "/To-Do-App-React/static/js/2.d787721d.chunk.js"
+  },
+  {
+    "revision": "1667039c22321bdff15b",
+    "url": "/To-Do-App-React/static/js/main.a19955d5.chunk.js"
+  },
+  {
+    "revision": "900a38f1ac499e8e3644",
+    "url": "/To-Do-App-React/static/js/runtime~main.51e3c896.js"
+  }
+]);
